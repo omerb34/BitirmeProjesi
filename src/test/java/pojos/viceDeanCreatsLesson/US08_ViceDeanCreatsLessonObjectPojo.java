c0f30afab0e8b04fee26ffd447a958d3598a1390
Omer Buyukergene <buyukergene@gotmail.com>
2023-04-29 12:35:33 +0300

    29.04.2023

---
+++ src/test/java/pojos/viceDeanCreatsLesson/US08_ViceDeanCreatsLessonObjectPojo.java
@@ -0,0 +1,60 @@
+package pojos.viceDeanCreatsLesson;
+
+public class US08_ViceDeanCreatsLessonObjectPojo {
+    private int lessonId;
+    private  String lessonName;
+    private  int creditScore;
+    private  boolean compulsory;
+
+    public US08_ViceDeanCreatsLessonObjectPojo() {
+    }
+
+    public US08_ViceDeanCreatsLessonObjectPojo(int lessonId, String lessonName, int creditScore, boolean compulsory) {
+        this.lessonId = lessonId;
+        this.lessonName = lessonName;
+        this.creditScore = creditScore;
+        this.compulsory = compulsory;
+    }
+
+    public int getLessonId() {
+        return lessonId;
+    }
+
+    public void setLessonId(int lessonId) {
+        this.lessonId = lessonId;
+    }
+
+    public String getLessonName() {
+        return lessonName;
+    }
+
+    public void setLessonName(String lessonName) {
+        this.lessonName = lessonName;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public boolean isCompulsory() {
+        return compulsory;
+    }
+
+    public void setCompulsory(boolean compulsory) {
+        this.compulsory = compulsory;
+    }
+
+    @Override
+    public String toString() {
+        return "US08_ViceDeanCreatsLessonObjectPojo{" +
+                "lessonId=" + lessonId +
+                ", lessonName='" + lessonName + '\'' +
+                ", creditScore=" + creditScore +
+                ", compulsory=" + compulsory +
+                '}';
+    }
+}
--- src/test/resources/features/apifeatures/ViceDeanCreatesLesson.feature
+++ src/test/resources/features/apifeatures/ViceDeanCreatesLesson.feature
@@ -1,6 +1,11 @@
-@omerb
+@apius08
 Feature: Vice Dean creates lessons
   Scenario:TC01_Create_Lesson
-    Given Kullanici olusturdugu dersi gormek icin bir get request gonderir
-    Then Kullanici olsurdugu ilk dersi gordugunu dogrular
+    Given Kullanici olusturdugu ilk dersi gormek icin bir get request gonderir
+    Then Kullanici olusturdugu ilk dersi gordugunu dogrular
+
+    Scenario: TC02_Create_Lesson_Without_Complusory
+      Given Kullanici olusturdugu ikinci dersi gormek icin bir get request gonderir
+      Then Kullanici olusturdugu ikinci dersi gordugunu dogrular
+
 
